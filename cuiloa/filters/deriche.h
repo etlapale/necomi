140039f11431b16fb14a6e76cd6df5411c077bf1
Émilien Tlapale <emilien@tlapale.com>
2014-08-25 17:18:37 -0700

    New cleaner version of deriche filter.

--- cuiloa/filters/deriche.h
+++ cuiloa/filters/deriche.h
@@ -25,217 +25,139 @@
 
 namespace cuiloa
 {
-  /**
-   * Filtering type used by the Canny-Deriche filter.
-   * \ingroup Filters
-   */
-  typedef enum {DERICHE_BLUR,
-		DERICHE_FIRST_DERIVATIVE,
-		DERICHE_SECOND_DERIVATIVE} DericheFilteringType;
 
-  /**
-   * Apply a Canny-Deriche recursive filter in-place on an array.
-   * \ingroup Filters
-   */
-  template <typename T, unsigned int n>
-    Array<T,n> &
-    deriche_filter(Array<T,n> & a, double sigma,
-		   DericheFilteringType type,
-		   int dim,
-		   bool cond = true)
-    {
-      const int* dims = a.dimensions();
+template <typename T, ArrayIndex N, ArrayIndex K>
+std::enable_if_t<K<N && 0<N && std::is_floating_point<T>::value,void>
+inner_loop(std::array<ArrayIndex,N>& path,
+           ArrayIndex dim,
+           bool cond,
+           Array<T,N>& a, T* y,
+           T a0, T a1, T a2, T a3,
+           T b1, T b2)
+{
+  auto& dims = a.dimensions();
 
-      if (a.is_empty() || sigma == 0.0 || dims[dim] <= 1)
-	{
-	  std::cerr << "cuiloa::deriche_filter(): Nothing to do" << std::endl;
-	  return a;
-	}
-      if (sigma < 0)
-	throw std::runtime_error("cuiloa::deriche_filter(): Invalid sigma");
-      
-      const double alpha = 1.695 / sigma;
-      const double exp_a = exp(alpha);
-      const double exp_na = exp(-alpha);
-      const double exp_n2a = exp_na * exp_na;
-      const double b1 = -2 * exp_na;
-      const double b2 = exp_n2a;
-      
-      double valk = 0, valkn = 0, a0 = 0, a1 = 0, a2 = 0, a3 = 0, g0 = 0;
-      double sumg1 = 0, sumg0 = 0;
-      
-      int parity = 0;
-
-      switch (type)
-	{
-	case DERICHE_BLUR:
-	  valk = (1 - exp_na) * (1 - exp_na) / (1 + 2*alpha*exp_na - exp_n2a);
-	  a0 = valk;
-	  a1 = valk * exp_na * (alpha - 1);
-	  a2 = valk * exp_na * (alpha + 1);
-	  a3 = -valk*exp_n2a;
-	  parity = 1;
-	  if (cond)
-	    {
-	      sumg1 = valk * (alpha * exp_a + exp_a - 1) / ((exp_a - 1) * (exp_a));
-	      g0 = valk;
-	      sumg0 = g0 + sumg1;
-	    }
-          else
-	    {
-	      g0 = sumg0 = sumg1 = 0;
-	    }
-	  break;
-	case DERICHE_FIRST_DERIVATIVE:
-          valk = -(1-exp_na)*(1-exp_na)*(1-exp_na)/(2*(exp_na+1)*exp_na);
-          a0 = a3 = 0;
-          a1 = valk*exp_na;
-          a2 = -valk*exp_na;
-          parity =-1;
-          if (cond)
-	    {
-	      sumg1 = (valk*exp_a)/((exp_a-1)*(exp_a-1));
-	      g0 = 0;
-	      sumg0 = g0+sumg1;
-	    }
-          else
-	    {
-	      g0 = sumg0 = sumg1 = 0;
-	    }
-          break;
-	case DERICHE_SECOND_DERIVATIVE:
-          valkn = (-2*(-1+3*exp_a-3*exp_a*exp_a+exp_a*exp_a*exp_a)/(3*exp_a+1+3*exp_a*exp_a+exp_a*exp_a*exp_a) );
-          valk = -(exp_n2a-1)/(2*alpha*exp_na);
-          a0 = valkn;
-          a1 = -valkn*(1+valk*alpha)*exp_na;
-          a2 = valkn*(1-valk*alpha)*exp_na;
-          a3 = -valkn*exp_n2a;
-          parity = 1;
-          if (cond)
-	    {
-	      sumg1 = valkn/2;
-	      g0 = valkn;
-	      sumg0 = g0+sumg1;
-	    }
-          else
-	    {
-	      g0 = sumg0 = sumg1 = 0;
-	    }
-	  break;
-	}
-      
-      double* y = new double[dims[dim]];
-      int stride = a.strides()[dim];
-      int* path = new int[n];
-      deriche_filter_inner_loop(0, path, dims,
-				dim, a, y, stride,
-				parity, b1, b2,
-				a0, a1, a2, a3,
-				g0, sumg0, sumg1);
-
-      return a;
+  if (K == dim) {
+    path[K] = 0;
+    inner_loop<T,N,K+1>(path, dim, cond,
+                        a, y, a0, a1, a2, a3, b1, b2);
+  }
+  else {
+    for (ArrayIndex i = 0; i < dims[K]; i++) {
+      path[K] = i;
+      inner_loop<T,N,K+1>(path, dim, cond,
+                          a, y, a0, a1, a2, a3, b1, b2);
     }
+  }
+}
+
+template <typename T, ArrayIndex N, ArrayIndex K>
+std::enable_if_t<K==N && 0<N && std::is_floating_point<T>::value,void>
+inner_loop(std::array<ArrayIndex,N>& path,
+           ArrayIndex dim,
+           bool cond,
+           Array<T,N>& a, T* y,
+           T a0, T a1, T a2, T a3,
+           T b1, T b2)
+{
+  auto& dims = a.dimensions();
+  auto ddim = dims[dim];
+  auto stride = a.strides()[dim];
   
-  /**
-   * This function is used by the Deriche filter function
-   * deriche_filter(Array<T>&,double,DericheFilteringType,int,bool)
-   * and should not be called by normal code.
-   */
-  template <typename T, unsigned int n>
-    void
-    deriche_filter_inner_loop(unsigned int level, int* path,
-			      const int* dims,
-			      unsigned int dim, cuiloa::Array<T,n>& array,
-			      double* y, int stride,
-			      int parity, double b1, double b2,
-			      double a0, double a1, double a2, double a3,
-			      double g0, double sumg0, double sumg1)
-    {
-      if (level < n)
-	{
-	  if (level == dim)
-	    {
-	      path[level] = 0;
-	      deriche_filter_inner_loop(level + 1, path,
-					dims, dim, array, y, stride,
-					parity, b1, b2, a0, a1, a2, a3, g0, sumg0, sumg1);
-	    }
-	  else
-	    {
-	      for (int i = 0; i < dims[level]; i++)
-		{
-		  path[level] = i;
-		  deriche_filter_inner_loop(level + 1, path,
-					    dims,
-					    dim, array, y, stride,
-					    parity, b1, b2, a0, a1, a2, a3, g0, sumg0, sumg1);
-		}
-	    }
-	  return;
-	}
-      
-      T* ima = array.data() + array.index(path);
-      T I1 = *ima;
-      T I0 = *(ima += stride);
-      double y2 = *(y++) = sumg0 * I1;
-      double y1 = *(y++) = g0 * I0 + sumg1 * I1;
-      
-      ima += stride;
-      for (int i=2; i<(int)dims[dim]; ++i)
-	{
-	  I0 = *ima;
-	  ima += stride;
-	  double Y0 = *(y++) = a0*I0 + a1*I1 - b1*y1 - b2*y2;
-	  I1 = I0;
-	  y2 = y1;
-	  y1 = Y0;
-	}
-      I1 = *(ima -= stride);
-      y2 = y1 = parity*sumg1*I1;
-      *ima = (T)(*(--y)+y2);
-      I0 = *(ima -= stride);
-      *ima = (T)(*(--y)+y1);
-      for (int ii=(int)dims[dim]-3; ii>=0; --ii)
-	{
-	  double Y0 = a2*I0 + a3*I1 - b1*y1 - b2*y2;
-	  I1 = I0;
-	  I0 = *(ima -= stride);
-	  *ima = (T) (*(--y) + Y0 );
-	  y2 = y1; y1 = Y0;
-	}
-    }
+  // Compute the index corresponding to the path
+  T* ima = &a(path);
 
-  /**
-   * Blur the given array along the specified dimension.
-   * The function modifies the given array.
-   * Calling this function is equivalent to use
-   * deriche_filter(a,sigma,DERICHE_BLUR,dim,cond)
-   *
-   * \param a   The array to be blurred.
-   * \param dim The dimension on which to blur.
-   * \return A reference to the blurred array.
-   * \ingroup Filters
-   */
-  template <typename T,unsigned int n>
-    Array<T,n>&
-    deriche_blur(Array<T,n>& a, unsigned int dim, double sigma, bool cond = true)
-    {
-      return deriche_filter(a, sigma, DERICHE_BLUR, dim, cond);
-    }
+  // First pass
+  double xp = 0, yp = 0, yb = 0;
+  if (cond) {
+    xp = *ima;
+    double coefp = (a0+a1)/(1+b1+b2);
+    yb = yp = coefp * xp;
+  }
+  for (ArrayIndex i = 0; i < ddim; i++) {
+    T xc = *ima;
+    ima += stride;
+    T yc = *(y++) = a0*xc + a1*xp - b1*yp - b2*yb;
+    xp = xc; yb = yp; yp = yc;
+  }
 
-  /**
-   * Blur the given array along all its dimensions.
-   * To blur only along a given dimension use either
-   * deriche_blur(Array<T>&,int,double,bool) or
-   * deriche_filter(Array<T>&,double,DericheFilteringType,int,bool)
-   * \ingroup Filters
-   */
-  template <typename T, unsigned int n>
-    Array<T,n>&
-    deriche_blur(Array<T,n>& a, double sigma)
-    {
-      for (ArrayIndex i = 0; i < n; i++)
-	deriche_blur(a, i, sigma);
-      return a;
-    }
+  // Second pass
+  T xn = 0, xa = 0, yn = 0, ya = 0;
+  if (cond) {
+    xn = xa = *(ima-stride);
+    T coefn = (a2+a3)/(1+b1+b2);
+    yn = ya = coefn * xn;
+  }
+  for (int i = ddim-1; i >= 0; i--) {
+    T xc = *(ima-=stride);
+    T yc = a2*xn + a3*xa - b1*yn - b2*ya;
+    xa = xn; xn = xc; ya = yn; yn = yc;
+    *ima = *(--y)+yc;
+  }
+}
+
+/**
+ * Filtering type used by the Canny-Deriche filter.
+ * \ingroup Filters
+ */
+typedef enum {DERICHE_BLUR} DericheFilteringType;
+
+template <typename T, ArrayIndex N>
+std::enable_if_t<0<N && std::is_floating_point<T>::value,Array<T,N>&>
+deriche(Array<T,N>& a, int dim, double sigma,
+        DericheFilteringType order=DERICHE_BLUR,
+        bool cond=true)
+{
+  // σ=0 is a nop
+  if (sigma == 0)
+    return a;
+  // σ must be >0
+  sigma = fabs(sigma);
+
+  T alpha = 1.695 / sigma;
+  T ena = exp (-alpha);
+  T ens = ena * ena;
+  T b1 = -2 * ena;
+  T b2 = ens;
+  
+  auto& dims = a.dimensions();
+
+  T a0, a1, a2, a3;
+
+  T k;
+  switch (order) {
+  case DERICHE_BLUR:
+    k = (1-ena) * (1-ena) / (1 + 2*alpha*ena - ens);
+    a0 =  k;
+    a1 =  k * ena * (alpha - 1);
+    a2 =  k * ena * (alpha + 1);
+    a3 = -k * ens;
+    break;
+  }
+
+  std::array<ArrayIndex,N> path;
+  T y[dims[dim]];
+
+  inner_loop<T,N,0>(path, dim, cond,
+	            a, y, a0, a1, a2, a3, b1, b2);
+
+  return a;
+}
+  
+/**
+ * Blur the given array along all its dimensions.
+ * To blur only along a given dimension use either
+ * deriche_blur(Array<T>&,int,double,bool) or
+ * deriche_filter(Array<T>&,double,DericheFilteringType,int,bool)
+ * \ingroup Filters
+ */
+template <typename T, ArrayIndex N>
+std::enable_if_t<0<N && std::is_floating_point<T>::value,Array<T,N>&>
+deriche_blur(Array<T,N>& a, double sigma)
+{
+  for (ArrayIndex i = 0; i < N; i++)
+    deriche_filter(a, i, sigma);
+  return a;
+}
+
 }
