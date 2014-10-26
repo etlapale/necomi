b3c8f5632128f2d19b70a4411380d041bc4a12de
Émilien Tlapale <emilien@tlapale.com>
2014-10-26 00:18:26 -0700

    Linear interpolation.

--- cuiloa/base/numerics.h
+++ cuiloa/base/numerics.h
@@ -57,6 +57,53 @@ namespace cuiloa
   {
     return discretization(min, max, size, [](auto&& a){ return a; });
   }
+  
+  enum class InterpolationMethod
+  {
+    NearestNeighbor,
+    Linear
+  };
+  
+  
+  template <InterpolationMethod method, typename T, typename Concrete>
+  class Interpolation
+  {
+  public:
+    Interpolation(const AbstractArray<Concrete,T,1>& a)
+      : m_array(a.shallow_copy())
+    {
+    }
+
+    template <typename U>
+    std::enable_if_t<method == InterpolationMethod::NearestNeighbor
+		     && std::is_arithmetic<U>::value, T>
+    operator()(U coord) const
+    {
+      auto idx = static_cast<cuiloa::ArrayIndex>(0.5 + coord);
+      return m_array(idx);
+    }
+
+    template <typename U>
+    std::enable_if_t<method == InterpolationMethod::Linear
+		     && std::is_arithmetic<U>::value, T>
+    operator()(U x) const
+    {
+      auto x0 = static_cast<cuiloa::ArrayIndex>(x);
+      auto y0 = m_array(x0);
+      auto y1 = m_array(x0 + 1);
+
+      return y0 + (y1 - y0)*(x - x0);
+    }
+
+  protected:
+    Concrete m_array;
+  };
+  
+  template <InterpolationMethod method, typename T, typename Concrete>
+  auto interpolation(cuiloa::AbstractArray<Concrete,T,1>& a)
+  {
+    return Interpolation<method,T,Concrete>(a);
+  }
 }
 
 // Local Variables:
--- tests/test-numerics.cc
+++ tests/test-numerics.cc
@@ -17,4 +17,28 @@ TEST_CASE( "basic numerics", "[base]" ) {
     REQUIRE( fabs(d2(90) - 0) < 2e-2 );
     REQUIRE( fabs(d2(180) + 1) < float_tol );
   }
+  
+  SECTION( "nearest-neighbor interpolation" ) {
+    auto a = linspace<double>(0, 10, 11);
+    auto i = interpolation<InterpolationMethod::NearestNeighbor>(a);
+    REQUIRE( i(0) == 0 );
+    REQUIRE( i(1) == 1 );
+    REQUIRE( i(10) == 10 );
+    REQUIRE( i(0.3) == 0 );
+    REQUIRE( i(0.8) == 1 );
+    REQUIRE( i(6.2) == 6 );
+    REQUIRE( i(6.9) == 7 );
+  }
+  
+  SECTION( "linear interpolation" ) {
+    auto a = linspace<double>(0, 10, 11);
+    auto i = interpolation<InterpolationMethod::Linear>(a);
+    REQUIRE( i(0) == 0 );
+    REQUIRE( i(1) == 1 );
+    REQUIRE( i(10) == 10 );
+    REQUIRE( i(0.3) == 0.3 );
+    REQUIRE( i(0.8) == 0.8 );
+    REQUIRE( i(6.2) == 6.2 );
+    REQUIRE( i(6.9) == 6.9 );
+  }
 }
