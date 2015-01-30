4c96242af368300d9c248b4ac24691ae060ce025
Émilien Tlapale <emilien@tlapale.com>
2015-01-30 13:17:20 -0800

    Documenting SDE EM solver parameters.

--- cuiloa/integrate/sde.h
+++ cuiloa/integrate/sde.h
@@ -19,64 +19,80 @@
 
 namespace cuiloa {
 
+/**
+ * Euler-Maruyama integration of stochastic differential equations.
+ * States are represented by immediate N-dimensional arrays, and
+ * the diffusion term can include an arbitrary number of noises.
+ */
+template <typename T, ArrayDimension N,
+	  typename Drift, typename Diffusion, typename PRNG>
+class EulerMaruyama
+{
+public:
   /**
-   * Euler-Maruyama integration of stochastic differential equations.
-   * States are represented by immediate N-dimensional arrays, and
-   * the diffusion term can include an arbitrary number of noises.
+   * Both drift and diffusion must be function objects taking the
+   * current state as argument, a const cuiloa::Array<T,N> instance,
+   * of dimensions given by \c dimensions [d1×…×dN].
+   * The drift must return an array of the same dimensions, [d1×…×dN],
+   * while the diffusion must return an array of adding an extra
+   * dimension corresponding to the noises, [d1×…×dN×dnum_noises].
    */
-  template <typename T, ArrayDimension N,
-	    typename Drift, typename Diffusion, typename PRNG>
-  class EulerMaruyama
+  EulerMaruyama(const Dimensions<N>& dimensions, ArrayIndex num_noises,
+                Drift drift, Diffusion diffusion, PRNG& prng)
+    : m_drift(drift), m_diffusion(diffusion),
+      m_t(0), m_X(dimensions),
+      m_num_noises(num_noises),
+      m_prng(prng)
   {
-  public:
-    EulerMaruyama(const Dimensions<N>& dimensions, ArrayIndex num_noises,
-		  Drift drift, Diffusion diffusion, PRNG& prng)
-      : m_drift(drift), m_diffusion(diffusion),
-	m_t(0), m_X(dimensions),
-	m_num_noises(num_noises),
-	m_prng(prng)
-    {
-      m_X.fill(0);
-    }
-
-    void step(T dt)
-    {
-      using namespace delayed;
-      using namespace delayed::broadcasting;
-
-      auto Winc = std::sqrt(dt)*normal(m_num_noises, m_prng);
-      m_X += dt*m_drift(m_X) + sum(m_diffusion(m_X)*Winc, 1);
-
-      m_t += dt;
-    }
-
-    T t() const
-    { return m_t; }
-
-    Array<T,N>& X()
-    { return m_X; }
-
-    const Array<T,N>& X() const
-    { return m_X; }
-
-  protected:
-    Drift m_drift;
-    Diffusion m_diffusion;
-    T m_t;
-    Array<T,N> m_X;
-    ArrayDimension m_num_noises;
-    PRNG& m_prng;
-  };
-
-  template <typename T, ArrayDimension N,
-	    typename Drift, typename Diffusion, typename PRNG>
-  auto euler_maruyama(const Dimensions<N>& dims, ArrayIndex num_noises,
-		      Drift drift, Diffusion diffusion,
-		      PRNG& prng)
-  {
-    return EulerMaruyama<T,N,Drift,Diffusion,PRNG>(dims, num_noises,
-						   drift, diffusion, prng);
+    m_X.fill(0);
   }
+
+  void step(T dt)
+  {
+    using namespace delayed;
+    using namespace delayed::broadcasting;
+
+    auto Winc = std::sqrt(dt)*normal(m_num_noises, m_prng);
+    m_X += dt*m_drift(m_X) + sum(m_diffusion(m_X)*Winc, 1);
+
+    m_t += dt;
+  }
+
+  T t() const
+  { return m_t; }
+
+  Array<T,N>& X()
+  { return m_X; }
+
+  const Array<T,N>& X() const
+  { return m_X; }
+
+protected:
+  Drift m_drift;
+  Diffusion m_diffusion;
+  /** Current time. */
+  T m_t;
+  /** Current state. */
+  Array<T,N> m_X;
+  /** Number of Wiener process noises. */
+  ArrayDimension m_num_noises;
+  /** Pseudo random number generator */
+  PRNG& m_prng;
+};
+
+/**
+ * Wrapper to the EulerMaruyama constructor.
+ * Allows to skip explicit template arguments.
+ */
+template <typename T, ArrayDimension N,
+	  typename Drift, typename Diffusion, typename PRNG>
+auto euler_maruyama(const Dimensions<N>& dims, ArrayIndex num_noises,
+		    Drift drift, Diffusion diffusion,
+		    PRNG& prng)
+{
+  return EulerMaruyama<T,N,Drift,Diffusion,PRNG>(dims, num_noises,
+                                                 drift, diffusion, prng);
+}
 
 }
 
