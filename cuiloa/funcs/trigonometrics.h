f06c05f5789e7c97d350d247820fccdc305e5039
Émilien Tlapale <emilien@tlapale.com>
2015-04-22 16:13:03 -0700

    Templates for trigonometrics.

--- cuiloa/funcs/trigonometrics.h
+++ cuiloa/funcs/trigonometrics.h
@@ -1,17 +1,7 @@
-/* Copyright © 2014–2015 University of California
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// cuiloa/base/trigonometrics.h – Scalar and delayed trigonometrics
+//
+// Copyright © 2014–2015 University of California, Irvine
+// Licensed under the Simplified BSD License.
 
 #pragma once
 
@@ -20,71 +10,71 @@
 namespace cuiloa
 {
 
-  /**
-   * Get the cosinus of each element interpreted as a angle in radians.
-   */
-  template <typename T, ArrayDimension N, typename Concrete,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  auto cos(const AbstractArray<Concrete,T,N>& a)
-  {
-    return make_delayed<T,N>(a.dimensions(),
-			     [a=a.shallow_copy()] (auto& coords) {
-			       return std::cos(a(coords));
-			     });
-  }
+/**
+ * Get the cosinus of each element interpreted as a angle in radians.
+ */
+template <typename Array,
+	  std::enable_if_t<is_array<Array>::value>* = nullptr>
+auto cos(const Array& a)
+{
+  using std::cos;
+  using C = decltype(cos(std::declval<typename Array::dtype>()));
+  return make_delayed<C,Array::ndim>(a.dimensions(), [a] (const auto& x) {
+      return std::cos(a(x));
+    });
+}
 
-  /**
-   * Get the cosinus of each element interpreted as a angle in radians.
-   */
-  template <typename T, ArrayDimension N, typename Concrete,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  auto sin(const AbstractArray<Concrete,T,N>& a)
-  {
-    return make_delayed<T,N>(a.dimensions(),
-			     [a=a.shallow_copy()] (auto& coords) {
-			       return std::sin(a(coords));
-			     });
-  }
+/**
+ * Get the sinus of each element interpreted as a angle in radians.
+ */
+template <typename Array,
+	  std::enable_if_t<is_array<Array>::value>* = nullptr>
+auto sin(const Array& a)
+{
+  using std::sin;
+  using C = decltype(sin(std::declval<typename Array::dtype>()));
+  return make_delayed<C,Array::ndim>(a.dimensions(), [a] (const auto& x) {
+      return sin(a(x));
+    });
+}
 
-  template <typename T,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  constexpr T radians(T angle)
-  {
-    return angle *  M_PI / 180.;
-  }
+template <typename T,
+	  std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
+constexpr T radians(T angle)
+{
+  return angle *  M_PI / 180.;
+}
 
-  /**
-   * Convert each element interpreted as a angle in degrees into radians.
-   */
-  template <typename T, ArrayDimension N, typename Concrete,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  auto radians(const AbstractArray<Concrete,T,N>& a)
-  {
-    return make_delayed<T,N>(a.dimensions(),
-			     [a=a.shallow_copy()] (auto& coords) {
-			       return radians(a(coords));
-			     });
-  }
-  
-  template <typename T,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  constexpr T degrees(T angle)
-  {
-    return angle * 180. / M_PI;
-  }
+/**
+ * Convert each element interpreted as an angle in degrees into radians.
+ */
+template <typename Array,
+	  std::enable_if_t<is_array<Array>::value>* = nullptr>
+auto radians(const Array& a)
+{
+  return make_delayed<typename Array::dtype,Array::ndim>(a.dimensions(), [a] (const auto& x) {
+      return radians(a(x));
+    });
+}
 
-  /**
-   * Convert each element interpreted as a angle in radians into degrees.
-   */
-  template <typename T, ArrayDimension N, typename Concrete,
-	    std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
-  auto degrees(const AbstractArray<Concrete,T,N>& a)
-  {
-    return make_delayed<T,N>(a.dimensions(),
-			     [a=a.shallow_copy()] (auto& coords) {
-			       return degrees(a(coords));
-			     });
-  }
+template <typename T,
+	  std::enable_if_t<std::is_floating_point<T>::value>* = nullptr>
+constexpr T degrees(T angle)
+{
+  return angle * 180. / M_PI;
+}
+
+/**
+ * Convert each element interpreted as a angle in radians into degrees.
+ */
+template <typename Array,
+	  std::enable_if_t<is_array<Array>::value>* = nullptr>
+auto degrees(const Array& a)
+{
+  return make_delayed<typename Array::dtype,Array::ndim>(a.dimensions(), [a] (const auto& x) {
+      return degrees(a(x));
+    });
+}
 
 } // namespace cuiloa
 
