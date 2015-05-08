d831be4c975e2b56689291d3b8dd71293f3f1ad5
Émilien Tlapale <emilien@tlapale.com>
2015-05-08 12:12:43 -0700

    Adding DynArray element accessors.

--- necomi/base/dynarray.h
+++ necomi/base/dynarray.h
@@ -21,7 +21,22 @@ public:
   template <typename ...Dims,
 	    std::enable_if_t<all_convertible<Dims..., dim_type>::value>* = nullptr>
   DynArray(Dims... dims)
-    : m_dims{static_cast<dim_type>(dims)...}
+    : DynArray(dims_type{static_cast<dim_type>(dims)...})
+  {
+  }
+
+  DynArray(const dims_type& dims)
+    : m_dims(dims)
+    , m_strides(default_strides(dims))
+    , m_shared_data(new T[size(*this)], [](T* p){ delete [] p; })
+    , m_data(m_shared_data.get())
+  {
+  }
+
+  DynArray(const DynArray<T>& a)
+    : m_strides(a.m_strides)
+    , m_shared_data(a.m_shared_data)
+    , m_data(a.m_data)
   {
   }
 
@@ -30,6 +45,43 @@ public:
 
   dim_type dim(dim_type i) const
   { return m_dims[i]; }
+
+  T& operator()(const dims_type& coords)
+  { return m_data[index(*this, coords)]; }
+  
+  const T& operator()(const dims_type& coords) const
+  { return m_data[index(*this, coords)]; }
+
+  template <typename ...Coords,
+	    std::enable_if_t<all_convertible<Coords...,dim_type>::value>* = nullptr>
+  T& operator()(Coords... coords)
+  {
+#ifndef NECOMI_NO_BOUND_CHECKS
+    if (sizeof...(Coords) != m_dims.size())
+      throw std::length_error(std::string("invalid number of coordinates (expecting ") + std::to_string(m_dims.size()) + ", got " + std::to_string(sizeof...(Coords)) + ")");
+#endif // NECOMI_NO_BOUND_CHECKS
+    
+    // TODO check indices for out of bounds
+    return m_data[index(*this, coords...)];
+  }
+
+  /// Return a raw pointer to the data associated with the array.
+  T* data()
+  { return m_data; }
+  
+  ///  Return the immutable data associated with the array.
+  const T* data() const
+  { return m_data; }
+  
+  /// Return a new shared pointer to the data.
+  std::shared_ptr<T> shared_data() const
+  {
+    return m_shared_data;
+  }
+
+  const dims_type& strides() const
+  { return m_strides; }
+
 protected:
   dims_type m_dims;
   dims_type m_strides;
--- tests/test-dynarray.cc
+++ tests/test-dynarray.cc
@@ -11,5 +11,60 @@ TEST_CASE( "dynamic arrays", "[core]" ) {
     std::vector<std::size_t> v{3,7,12,5};
     REQUIRE( a.dims() == v );
     REQUIRE( size(a) == 1260 );
+
+    DynArray<double> b(3,4);
+    std::vector<std::size_t> w{3,4};
+    REQUIRE( b.dims() == w );
+    REQUIRE( size(b) == 12 );
+  }
+
+  SECTION( "data access" ) {
+    DynArray<int> a0;
+    a0() = 123;
+    REQUIRE( a0() == 123 );
+    REQUIRE( a0.data()[0] == 123 );
+
+#ifndef NECOMI_NO_BOUND_CHECKS
+    bool exception_thrown = false;
+    try {
+      a0(1);
+    } catch (std::length_error&) {
+      exception_thrown = true;
+    }
+    REQUIRE( exception_thrown );
+#endif // NECOMI_NO_BOUND_CHECKS
+    
+    DynArray<int> a1(127);
+    a1(32) = 123;
+    a1(69) = 456;
+    REQUIRE( a1(32) == 123 );
+    REQUIRE( a1(69) == 456 );
+    REQUIRE( a1.data()[32] == 123 );
+    REQUIRE( a1.data()[69] == 456 );
+
+    DynArray<double> a2(3,4);
+    for (unsigned int i = 0; i < size(a2); i++)
+      a2.data()[i] = i;
+    REQUIRE( a2(1,2) == 6 );
+    REQUIRE( a2(2,3) == index(a2, 2, 3) );
+
+#ifndef NECOMI_NO_BOUND_CHECKS
+    exception_thrown = false;
+    try {
+      a2(1);
+    } catch (std::length_error&) {
+      exception_thrown = true;
+    }
+    REQUIRE( exception_thrown );
+
+    exception_thrown = false;
+    try {
+      a2(1,2,3);
+    } catch (std::length_error&) {
+      exception_thrown = true;
+    }
+    REQUIRE( exception_thrown );
+#endif // NECOMI_NO_BOUND_CHECKS
+
   }
 }
