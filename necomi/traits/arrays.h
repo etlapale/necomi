61f4828218d116fa9319af3429d872e053daa54f
Émilien Tlapale <emilien@tlapale.com>
2015-06-02 14:50:15 -0700

    Removing old comments.

--- necomi/traits/arrays.h
+++ necomi/traits/arrays.h
@@ -35,38 +35,12 @@ template <typename T> struct has_strides<T, decltype(&T::strides, void())>
   : std::true_type {};
 
 
-/**
- * \anchor Array
- *
- * Check if a given type is a necomi array.
- * A necomi array must define the following types:
- * - \c dtype	Type of the elements.
- *
- * An array must also define the following compile-time constants:
- * - \c ndim	The number of dimensions in the array.
- *
- * The following runtime members must be accessible:
- * - \c dimensions	Dimensions as \c std::array<std::size_t,ndim>.
- *
- * A minimal array class definition might hence look like:
- * \code{.cpp}
- * struct MyArray {
- *   using dtype = double;
- *   enum { ndim = 1 };
- *   Dimensions<ndim> dimensions;
- * };
- * \endcode
- *
- * \note The standard library define a similar function
- *       for standard array types.
- */
 template <typename T>
 struct is_array
   : std::integral_constant<bool,
 			   has_dtype<T>::value
 			   && has_ndim<T>::value
 			   && has_dims<T>::value>
-  // TODO: check dimensions type
 {};
   
 /**
@@ -80,38 +54,13 @@ struct is_same_ndim_dtype
 			   && Array1::ndim == Array2::ndim>
 {};
 
-/**
- * \struct necomi::is_indexable<T>
- * \anchor IndexableArray
- *
- * Check if individual array elements are accessible.
- *
- * An indexable array provides a Coordinates-based access to its
- * elements. It must also be an array satisfying the conditions of
- * \ref is_array. Elements are accessed through an <tt>operator()
- * const</tt> taking a constant \c Coordinates<ndim> parameter and
- * returning a value of the array element type (castable to \c
- * dtype).
- *
- * A minimal array class \c MyArray might be extended to an indexable
- * one by adding the adequate operator:
- * \code{.cpp}
- * struct MyIndexableArray : MyArray {
- *   dtype operator()(const Coordinates<ndim>&) const
- *   { return 42; };
- * };
- * \endcode
- * \ingroup Concepts
- *
- * \see is_array
- */
 template <typename T, typename = void>
 struct is_indexable : std::false_type {};
   
 template <typename T>
 struct is_indexable<T, std::enable_if_t<is_array<T>::value,void>>
   : std::integral_constant<bool,
-			   is_callable<const T,const typename T::dims_type&>::value>
+			   is_callable<T,const typename T::dims_type&>::value>
 {};
 
 template <typename T, typename = void>
