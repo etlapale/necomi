f140375f517b2089399f9f7cf1004fb796ad8cb5
Émilien Tlapale <emilien@tlapale.com>
2015-08-17 22:21:38 -0700

    Checking exponential filter output coefficient.

--- tests/test-filters-exponential.cc
+++ tests/test-filters-exponential.cc
@@ -11,6 +11,8 @@ using namespace necomi;
 
 #ifdef HAVE_BOOST
 
+static const double epsilon = 1e-10;
+
 SCENARIO( "exponential filters are recursive filters", "[filters]" ) {
   GIVEN( "an exponential cascade filter" ) {
     auto n = 8UL;
@@ -27,24 +29,32 @@ SCENARIO( "exponential filters are recursive filters", "[filters]" ) {
     }
 
     WHEN( "an exponential filter of order 8 and bandwidth 85.0 is constructed") {
+      
+      std::vector<double> coefs = {1.0,
+				   -8.1915810880534128,
+				   29.823111432068593,
+				   -63.336631450255496,
+				   86.471192061486988,
+				   -78.703975728034578,
+				   47.756296231732065,
+				   -18.628551097492043,
+				   4.238813524112568,
+				   -0.42867388518401917};
+      
       THEN( "its input coefficients are fixed" ) {
-	std::vector<double> coefs = {1.0,
-				     -8.1915810880534128,
-				     29.823111432068593,
-				     -63.336631450255496,
-				     86.471192061486988,
-				     -78.703975728034578,
-				     47.756296231732065,
-				     -18.628551097492043,
-				     4.238813524112568,
-				     -0.42867388518401917};
 	REQUIRE( coefs.size() == filter.a().size() );
 	
 	double err = 0;
 	for (auto i = 0UL; i < coefs.size(); i++)
 	  err += std::abs(coefs[i] - filter.a()[i]);
 
-	REQUIRE( err < 1e-5 );
+	REQUIRE( err < epsilon );
+      }
+
+      THEN( "its output coefficient is fixed" ) {
+	auto sum = std::accumulate(coefs.begin(), coefs.end(), 0.0);
+	REQUIRE( std::abs(sum - 3.8066805263525794e-10) < epsilon );
+	REQUIRE( std::abs(sum - filter.b()[0]) < epsilon );
       }
     }
   }
