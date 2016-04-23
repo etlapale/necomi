e7d127cc05745c21d358807f9f909458ce52f98e
Émilien Tlapale <emilien@tlapale.com>
2016-04-23 22:29:10 +0200

    Better coverage for transform().

--- tests/test-algos-modif.cc
+++ tests/test-algos-modif.cc
@@ -13,5 +13,22 @@ TEST_CASE( "modifying array operations", "[algorithms]" ) {
     REQUIRE( a(3) == -3 );
     REQUIRE( a(4) == -4 );
     REQUIRE( a(7) == -7 );
+
+    auto b = strided(range<int>(10));
+    auto c = strided(range<int>(10));
+    transform(b, c, std::plus<>());
+
+    REQUIRE( b(0) == 0 );
+    REQUIRE( b(1) == 2 );
+    REQUIRE( b(7) == 14 );
+
+    auto d = strided(range<int>(8));
+    bool length_error_thrown = false;
+    try {
+      transform(c, d, std::plus<>());
+    } catch (std::length_error&) {
+      length_error_thrown = true;
+    }
+    REQUIRE( length_error_thrown );
   }
 }
